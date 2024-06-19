b56d0af28d877ba139804b8f97a18bf41395e4b2
Ilya <aquarell.dev@gmail.com>
2024-06-19 14:31:36 +0300

    added colors theme

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,1 +1,13 @@
-export const COLORS = {}
+export const COLORS = {
+	background: {
+		primary: "#1b2627",
+		secondary: "#202c2d",
+	},
+	accent: {
+		primary: "#b2f35f",
+	},
+	text: {
+		primary: "#fff",
+		secondary: "#879798",
+	},
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {},
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: {
+					primary: "#1b2627",
+					secondary: "#202c2d",
+				},
+				accent: {
+					primary: "#b2f35f",
+				},
+				text: {
+					primary: "#fff",
+					secondary: "#879798",
+				},
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
